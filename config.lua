fb76ecba9b79fecaf220a83d4f5730575a4f2273
Monsler <105060825+Monsler@users.noreply.github.com>
2024-08-23 15:17:57 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,7 +8,7 @@ application =
 	content =
 	{
 		width = 720,
-		height = 720*display.pixelWidth/display.pixelHeight, 
+		height = 720*display.pixelHeight/display.pixelWidth, 
 		scale = "letterbox",
 		fps = 60,
 		
