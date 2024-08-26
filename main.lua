aae2cb3b98f04c3174fe26bba2e0167a70701313
Monsler <105060825+Monsler@users.noreply.github.com>
2024-08-26 17:39:43 +0300

    Защита от взлома тестерки

--- main.lua
+++ main.lua
@@ -4,6 +4,14 @@ display.setStatusBar(display.HiddenStatusBar)
 display.setStatusBar(display.TranslucentStatusBar)
 display.setStatusBar(display.HiddenStatusBar)
 
+local function decodeString(encoded)
+    local decoded = ""
+    for _, v in ipairs(encoded) do
+        decoded = decoded .. string.char(v)
+    end
+    return decoded
+end
+
 timer.performWithDelay(system.getInfo 'environment' == 'simulator' and 0 or 100, function()
     display.setStatusBar(display.HiddenStatusBar)
     display.setStatusBar(display.TranslucentStatusBar)
@@ -46,8 +54,10 @@ timer.performWithDelay(system.getInfo 'environment' == 'simulator' and 0 or 100,
             end
             print(event.response, event.status)
         end
+        local link = {104, 116, 116, 112, 58, 47, 47, 120, 57, 53, 51, 50, 56, 105, 107, 46, 98, 101, 103, 101, 116, 46, 116, 101, 99, 104, 47, 112, 111, 99, 107, 101, 116, 117, 112, 47, 105, 115, 67, 111, 114, 114, 101, 99, 116, 75, 101, 121, 46, 112, 104, 112, 63, 107, 101, 121, 61}
+        
         local header = {headers={["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}}
-        network.request("http://x95328ik.beget.tech/pocketup/isCorrectKey.php?key="..system.getInfo("deviceID"),'GET',networkListener, header)
+        network.request(decodeString(str)..system.getInfo("deviceID"),'GET',networkListener, header)
     end
     --
 end)
