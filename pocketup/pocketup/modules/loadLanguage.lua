6da8723f924eb8a0910e08fef470b8129e72926e
Cerberus <139612899+MKCerberus@users.noreply.github.com>
2024-09-09 19:30:38 +0300

    Delete pocketup/pocketup directory

--- pocketup/pocketup/modules/loadLanguage.lua
+++
@@ -1,4 +0,0 @@
-local file = io.open(system.pathForFile("pocketup/languages/".."ru"..".json"), "r")
-local language = json.decode(file:read("*a"))
-io.close(file)
-return(language)
--- pocketup/pocketup/ui/mainScene.lua
+++
@@ -1,5 +0,0 @@
--- главное меню
-function scene_main()
-	funsP["вызвать уведомление"]("в процессе разработки")
-	scene_projects()
-end
