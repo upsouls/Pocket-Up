d986739a9b55d78f8982af112e4639f5882a7f76
Max-Dil <kleska16@gmail.com>
2024-12-17 19:25:35 +0300

    Перестройка переменные

--- pocketup/gameAndBlocks/launchBlocks_other.lua
+++ pocketup/gameAndBlocks/launchBlocks_other.lua
@@ -63,40 +63,6 @@ local function make_block(infoBlock, object, images, sounds, make_all_formulas, 
         lua = lua..
         "for i=1, type("..rep..") == 'number' and "..rep.." or 0 do\
         threadFun.wait(type("..time..") == 'number' and ("..time.."*1000) or 0)"
-    elseif nameBlock == 'setVariable' and infoBlock[2][1][2]~=nil then
-        local value = make_all_formulas(infoBlock[2][2], object)
-        add_pcall()
-        if infoBlock[2][1][1] == 'globalVariable' then
-            lua = lua..'var_'..infoBlock[2][1][2]..' = '..value..'\n'
-            lua = lua..
-            'if varText_'..infoBlock[2][1][2]..' then\
-            varText_'..infoBlock[2][1][2]..'.text = type(var_'..infoBlock[2][1][2]..')=="boolean" and (var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(var_'..infoBlock[2][1][2]..')=="table" and encodeList(var_'..infoBlock[2][1][2]..') or var_'..infoBlock[2][1][2]..'\
-            end'
-        else
-            lua = lua..'target.var_'..infoBlock[2][1][2]..' = '..value..'\n'
-            lua = lua..
-            'if target.varText_'..infoBlock[2][1][2]..' then\
-            target.varText_'..infoBlock[2][1][2]..'.text = type(target.var_'..infoBlock[2][1][2]..')=="boolean" and (target.var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(target.var_'..infoBlock[2][1][2]..')=="table" and encodeList(target.var_'..infoBlock[2][1][2]..') or target.var_'..infoBlock[2][1][2]..'\
-            end'
-        end
-        end_pcall()
-    elseif nameBlock == 'editVariable' and infoBlock[2][1][2]~=nil then
-        local value = make_all_formulas(infoBlock[2][2], object)
-        add_pcall()
-        if infoBlock[2][1][1] == 'globalVariable' then
-            lua = lua..'var_'..infoBlock[2][1][2]..' = type(var_'..infoBlock[2][1][2]..')=="boolean" and (var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(var_'..infoBlock[2][1][2]..')=="table" and encodeList(var_'..infoBlock[2][1][2]..') or var_'..infoBlock[2][1][2]..'+('..make_all_formulas(infoBlock[2][2], object)..')\n'
-            lua = lua..
-            'if varText_'..infoBlock[2][1][2]..' then\
-            varText_'..infoBlock[2][1][2]..'.text = var_'..infoBlock[2][1][2]..'\
-            end'
-        else
-            lua = lua..'target.var_'..infoBlock[2][1][2]..' = target.var_'..infoBlock[2][1][2]..' + '..value..'\n'
-            lua = lua..
-            'if target.varText_'..infoBlock[2][1][2]..' then\
-            target.varText_'..infoBlock[2][1][2]..'.text = type(target.var_'..infoBlock[2][1][2]..')=="boolean" and (target.var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(target.var_'..infoBlock[2][1][2]..')=="table" and encodeList(target.var_'..infoBlock[2][1][2]..') or target.var_'..infoBlock[2][1][2]..'\
-            end'
-        end
-        end_pcall()
     elseif nameBlock == 'openLink' then
         local link = make_all_formulas(infoBlock[2][1], object)
         add_pcall()
@@ -117,106 +83,6 @@ events_touchScreen[key][i](value.clones[i2])\
 end\
 end\
 end'
-    elseif nameBlock == 'showVariable' and infoBlock[2][1][2]~=nil then
-        local x = make_all_formulas(infoBlock[2][2], object)
-        local y = make_all_formulas(infoBlock[2][3], object)
-        add_pcall()
-        if infoBlock[2][1][1] == 'globalVariable' then
-            lua = lua..'if (varText_'..infoBlock[2][1][2]..'~=nil and varText_'..infoBlock[2][1][2]..'.text~=nil) then\ndisplay.remove(varText_'..infoBlock[2][1][2]..')\nend\nvarText_'..infoBlock[2][1][2]..' = display.newText(type(var_'..infoBlock[2][1][2]..')=="boolean" and (var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(var_'..infoBlock[2][1][2]..')=="table" and encodeList(var_'..infoBlock[2][1][2]..') or var_'..infoBlock[2][1][2]..', '..x..', -'..y..', "fonts/font_1", 30)\n'
-            lua = lua..'varText_'..infoBlock[2][1][2]..':setFillColor(0, 0, 0)'
-            lua = lua.."\ncameraGroup:insert(varText_"..infoBlock[2][1][2]..")"
-        else
-            lua = lua..'if (target.varText_'..infoBlock[2][1][2]..'~=nil and target.varText_'..infoBlock[2][1][2]..'.text~=nil) then\ndisplay.remove(target.varText_'..infoBlock[2][1][2]..')\nend\ntarget.varText_'..infoBlock[2][1][2]..' = display.newText(type(target.var_'..infoBlock[2][1][2]..')=="boolean" and (target.var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(target.var_'..infoBlock[2][1][2]..')=="table" and encodeList(target.var_'..infoBlock[2][1][2]..') or target.var_'..infoBlock[2][1][2]..', '..x..', -'..y..', "fonts/font_1", 30)\n'
-            lua = lua..'target.varText_'..infoBlock[2][1][2]..':setFillColor(0, 0, 0)'
-            lua = lua.."\ncameraGroup:insert(target.varText_"..infoBlock[2][1][2]..")"
-        end
-        end_pcall()
-    elseif nameBlock == 'showVariable2' and infoBlock[2][1][2]~=nil then
-        local x = make_all_formulas(infoBlock[2][2], object)
-        local y = make_all_formulas(infoBlock[2][3], object)
-        local size = make_all_formulas(infoBlock[2][4], object)
-        local hex = make_all_formulas(infoBlock[2][5], object)
-        local aligh = infoBlock[2][6][2]
-        add_pcall()
-        if infoBlock[2][1][1] == 'globalVariable' then
-            lua = lua..'if (varText_'..infoBlock[2][1][2]..'~=nil and varText_'..infoBlock[2][1][2]..'.text~=nil) then\ndisplay.remove(varText_'..infoBlock[2][1][2]..')\nend\nvarText_'..infoBlock[2][1][2]..' = display.newText({text = type(var_'..infoBlock[2][1][2]..')=="boolean" and (var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(var_'..infoBlock[2][1][2]..')=="table" and encodeList(var_'..infoBlock[2][1][2]..') or var_'..infoBlock[2][1][2]..', align="'..aligh..'", x = '..x..', y = - '..y..', font = nil, fontSize = 30 *('..size..'/100) })\n'
-            lua = lua..'local rgb = utils.hexToRgb('..hex..')\n'
-            lua = lua..'varText_'..infoBlock[2][1][2]..':setFillColor(rgb[1], rgb[2], rgb[3])'
-            lua = lua.."\ncameraGroup:insert(varText_"..infoBlock[2][1][2]..")"
-        else
-            lua = lua..'if (target.varText_'..infoBlock[2][1][2]..'~=nil and target.varText_'..infoBlock[2][1][2]..'.text~=nil) then\ndisplay.remove(target.varText_'..infoBlock[2][1][2]..')\nend\ntarget.varText_'..infoBlock[2][1][2]..' = display.newText({text = type(target.var_'..infoBlock[2][1][2]..')=="boolean" and (target.var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type(target.var_'..infoBlock[2][1][2]..')=="table" and encodeList(target.var_'..infoBlock[2][1][2]..') or target.var_'..infoBlock[2][1][2]..', align="'..aligh..'", x = '..x..', y = - '..y..', font = nil, fontSize = 30 *('..size..'/100) })\n'
-            lua = lua..'local rgb = utils.hexToRgb('..hex..')\n'
-            lua = lua..'target.varText_'..infoBlock[2][1][2]..':setFillColor(rgb[1], rgb[2], rgb[3])'
-            lua = lua.."\ncameraGroup:insert(target.varText_"..infoBlock[2][1][2]..")"
-        end
-        end_pcall()
-    elseif nameBlock == 'hideVariable' and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        if infoBlock[2][1][1] == 'globalVariable' then
-            lua = lua..'display.remove(varText_'..infoBlock[2][1][2]..')\nvarText_'..infoBlock[2][1][2]..' = nil'
-        else
-            lua = lua..'display.remove(target.varText_'..infoBlock[2][1][2]..')\ntarget.varText_'..infoBlock[2][1][2]..' = nil'
-        end
-        end_pcall()
-    elseif nameBlock=="removeVariableCamera" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua.."if ("..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2].."~=nil and "..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..".text ~= nil) then\nnotCameraGroup:insert("..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..")\nend"
-        end_pcall()
-     elseif nameBlock=="insertVariableCamera" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua.."if ("..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2].."~=nil and "..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..".text ~= nil) then\ncameraGroup:insert("..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..")\nend"
-        end_pcall()
-    elseif nameBlock=="saveVariable" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..'local arrayVariables = plugins.json.decode(funsP["получить сохранение"]("'..(infoBlock[2][1][1]=="globalVariable" and app.idProject or obj_path)..'/variables"))'
-        lua = lua..'\nfor i=1, #arrayVariables do\nif (arrayVariables[i][1]=='..infoBlock[2][1][2]..') then\narrayVariables[i][3] = '..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..'\nfunsP["записать сохранение"]("'..(infoBlock[2][1][1]=="globalVariable" and app.idProject or obj_path)..'/variables", plugins.json.encode(arrayVariables))\nbreak\nend\nend'
-        end_pcall()
-    elseif nameBlock=="readVariable" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..'local arrayVariables = plugins.json.decode(funsP["получить сохранение"]("'..(infoBlock[2][1][1]=="globalVariable" and app.idProject or obj_path)..'/variables"))'
-        lua = lua..'\nfor i=1, #arrayVariables do\nif (arrayVariables[i][1]=='..infoBlock[2][1][2]..') then\n'..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..'= arrayVariables[i][3]~=nil and arrayVariables[i][3] or 0\nbreak\nend\nend'
-        lua = lua..'\nif '..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'varText_'..infoBlock[2][1][2]..' then\n '..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'varText_'..infoBlock[2][1][2]..'.text = type('..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..')=="boolean" and ('..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..' and app.words[373] or app.words[374]) or type('..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..')=="table" and encodeList('..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..') or '..(infoBlock[2][1][1]=="globalVariable" and '' or 'target.')..'var_'..infoBlock[2][1][2]..'\nend'
-        end_pcall()
-    elseif nameBlock=="addElementArray" and infoBlock[2][2][2]~=nil then
-        add_pcall()
-        lua = lua..(infoBlock[2][2][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][2][2].."[#"..(infoBlock[2][2][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][2][2].."+1] = "..make_all_formulas(infoBlock[2][1], object)
-        end_pcall()
-    elseif nameBlock=="deleteElementArray" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua.."table.remove("..(infoBlock[2][1][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][1][2]..", "..make_all_formulas(infoBlock[2][2],object)..")"
-        end_pcall()
-    elseif nameBlock=="deleteAllElementsArray" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..(infoBlock[2][1][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][1][2].." = {}"
-        end_pcall()
-    elseif nameBlock=="pasteElementArray" and infoBlock[2][2][2]~=nil then
-        add_pcall()
-         lua = lua.."if ("..make_all_formulas(infoBlock[2][3], object).."<=#"..(infoBlock[2][2][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][2][2].."+1 and "..make_all_formulas(infoBlock[2][3], object)..">0) then\ntable.insert("..(infoBlock[2][2][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][2][2]..", "..make_all_formulas(infoBlock[2][3], object)..", "..make_all_formulas(infoBlock[2][1], object)..")\nend"
-        end_pcall()
-    elseif nameBlock=="replaceElementArray" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua.."if ("..make_all_formulas(infoBlock[2][2], object).."<=#"..(infoBlock[2][1][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][1][2].."+1 and "..make_all_formulas(infoBlock[2][2], object)..">0) then\n"..(infoBlock[2][1][1]=="globalArray" and "" or "target.").."list_"..infoBlock[2][1][2].."["..make_all_formulas(infoBlock[2][2],object).."] = "..make_all_formulas(infoBlock[2][3],object).."\nend"
-        end_pcall()
-    elseif nameBlock=="saveArray" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..'local arrayArrays = plugins.json.decode(funsP["получить сохранение"]("'..(infoBlock[2][1][1]=="globalArray" and app.idProject or obj_path)..'/arrays"))'
-        lua = lua..'\nfor i=1, #arrayArrays do\nif (arrayArrays[i][1]=='..infoBlock[2][1][2]..') then\narrayArrays[i][3] = '..(infoBlock[2][1][1]=="globalArray" and '' or 'target.')..'list_'..infoBlock[2][1][2]..'\nfunsP["записать сохранение"]("'..(infoBlock[2][1][1]=="globalArray" and app.idProject or obj_path)..'/arrays", plugins.json.encode(arrayArrays))\nbreak\nend\nend\n'
-        end_pcall()
-    elseif nameBlock=="readArray" and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..'local arrayArrays = plugins.json.decode(funsP["получить сохранение"]("'..(infoBlock[2][1][1]=="globalArray" and app.idProject or obj_path)..'/arrays"))'
-        lua = lua..'\nfor i=1, #arrayArrays do\nif (arrayArrays[i][1]=='..infoBlock[2][1][2]..') then\n'..(infoBlock[2][1][1]=="globalArray" and '' or 'target.')..'list_'..infoBlock[2][1][2]..'= arrayArrays[i][3]~=nil and arrayArrays[i][3] or {}\nbreak\nend\nend'
-        end_pcall()
-    elseif nameBlock=="columnStorageToArray" and infoBlock[2][3][2]~=nil then
-        add_pcall()
-        lua = lua.."local allArraysValues = plugins.json.decode('[\"'.."..make_all_formulas(infoBlock[2][2], object)..":gsub('\"','\\\\\"'):gsub('\\r\\n','\",\"'):gsub('\\n','\",\"')..'\"]')"
-        lua = lua.."\nfor i=1, #allArraysValues do\nlocal values = plugins.json.decode('[\"'..allArraysValues[i]:gsub('\\\"','\\\\\"'):gsub(',','\",\"')..'\"]')\nallArraysValues[i] = values["..make_all_formulas(infoBlock[2][1], object).."]==nil and '' or values["..make_all_formulas(infoBlock[2][1], object).."]\nend"
-        lua = lua.."\n"..(infoBlock[2][3][1]=="globalArray" and '' or 'target.').."list_"..infoBlock[2][3][2].." = allArraysValues"
-        end_pcall()
-    elseif nameBlock=="getRequest" and infoBlock[2][2][2]~=nil then
-        add_pcall()
-        lua = lua.."local function networkListener(event)\nif (mainGroup~=nil and mainGroup.x~=nil) then\n"..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."var_"..infoBlock[2][2][2].." = event.response\nif ("..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][2][2]..") then\n"..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][2][2]..".text = type("..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."var_"..infoBlock[2][2][2]..")=='boolean' and ("..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."var_"..infoBlock[2][2][2].." and app.words[373] or app.words[374]) or type("..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."var_"..infoBlock[2][2][2]..")=='table' and encodeList("..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."var_"..infoBlock[2][2][2]..") or "..(infoBlock[2][2][1]=="globalVariable" and "" or "target.").."var_"..infoBlock[2][2][2].."\nend\nend\nend\nlocal header = {headers={[\"User-Agent\"] = \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36\"}}\n\nnetwork.request("..make_all_formulas(infoBlock[2][1],object)..",'GET',networkListener, header)"
-        end_pcall()
     elseif nameBlock == 'showToast' then
         add_pcall()
         local arg1 = make_all_formulas(infoBlock[2][1], object)
@@ -236,18 +102,6 @@ end'
         add_pcall()
         local arg1 = make_all_formulas(infoBlock[2][1], object)
         lua = lua.."target.physicsTable.outline = graphics.newOutline("..arg1..", target.image_path, system.DocumentsDirectory)\ntarget:physicsReload()\n"
-        end_pcall()
-    elseif nameBlock == 'setAnchorVariable' and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..".anchorX, "..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..".anchorY = "..make_all_formulas(infoBlock[2][2], object).."/100, "..make_all_formulas(infoBlock[2][3], object).."/100"
-        end_pcall()
-    elseif nameBlock == 'toFrontLayerVar' and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..":toFront()"
-        end_pcall()
-    elseif nameBlock == 'toBackLayerVar' and infoBlock[2][1][2]~=nil then
-        add_pcall()
-        lua = lua..(infoBlock[2][1][1]=="globalVariable" and "" or "target.").."varText_"..infoBlock[2][1][2]..":toBack()"
         end_pcall()
     elseif nameBlock == 'removeAdaptiveSizeDevice' then
         add_pcall()
